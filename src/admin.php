36e7541c659d1221ca91a68ce13d71ee2519c36a
vuthihonggam <you@example.com>
2022-01-09 17:52:44 +0700

    fix hien thi

--- src/admin.php
+++ src/admin.php
@@ -1,71 +1,82 @@
-<?php
-    // Trước khi cho người dùng xâm nhập vào bên trong
-    // Phải kiểm tra THẺ LÀM VIỆC
-    session_start();
-    if(!isset($_SESSION['isLoginOK'])){
-        header("location:login.php");
-    }
-    
-    require "template/header.php";
-?>
-    <main>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DỰ ÁN</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+
         <div class="container">
-            <h5 class="text-center text-primary mt-5">Dự án khí tượng thủy văn, biến đổi khí hậu</h5>
-            <div>
-                <a class="btn btn-primary" href="add_employee.php">Thêm</a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Dự án</a>
+                </div>
             </div>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Maduan</th>
-                        <th scope="col">Họ và tên</th>
-                        <th scope="col">nam thuc hien</th>
-                        <th scope="col">linh vuc</th>
-                        <th scope="col">nhiem vu</th>
-                        <th scope="col">co quan thuc hien</th>
-                        <th scope="col">Sửa</th>
-                        <th scope="col">Xóa</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <!-- Vùng này là Dữ liệu cần lặp lại hiển thị từ CSDL -->
-                    <?php
-                        // Bước 01: Kết nối Database Server
-                        $conn = mysqli_connect('localhost','root','','bdkh');
-                        if(!$conn){
-                            die("Kết nối thất bại. Vui lòng kiểm tra lại các thông tin máy chủ");
+            </nav>
+        </div>
+    </header>
+    <main>
+    <div class="container">
+        <h5 class="text-center text-primary mt-5">Dự án khí tượng thủy văn, biến đổi khí hậu</h5>
+        <div>
+            <a class="btn btn-primary" href="add.php">Thêm</a>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Mã dự án</th>
+                    <th scope="col">Tên dự án </th> 
+                    <th scope="col">Năm thực hiện</th>
+                    <th scope="col">lĩnh vực</th>
+                    <th scope="col">Nhiệm vụ</th>
+                    <th scope="col">Cơ quan thực hiện</th>
+                    <th scope="col">Sửa</th>
+                    <th scope="col">Xóa</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Vùng này là Dữ liệu cần lặp lại hiển thị từ CSDL -->
+                <?php
+                    // Bước 01: Kết nối Database Server
+                    $conn = mysqli_connect('localhost','root','','1951064072_kttv_bdkh');
+                    if(!$conn){
+                        die("Kết nối thất bại. Vui lòng kiểm tra lại các thông tin máy chủ");
+                    }
+                    // Bước 02: Thực hiện truy vấn
+                    $sql = "SELECT * FROM duan ";
+                    $result = mysqli_query($conn,$sql);
+                    // Bước 03: Xử lý kết quả truy vấn
+                    if(mysqli_num_rows($result) > 0){
+                        while($row = mysqli_fetch_assoc($result)){
+                ?>
+                            <tr>
+                                <th scope="row"><?php echo $row['maduan']; ?></th>
+                                <td><?php echo $row['tenduan']; ?></td>
+                                <td><?php echo $row['namthuhien']; ?></td>
+                                <td><?php echo $row['linhvuc']; ?></td>
+                                <td><?php echo $row['nhiemvu']; ?></td>
+                                <td><?php echo $row['coquanthuchien']; ?></td>
+                                <td><a href="update.php?maduan=<?php echo $row['maduan']; ?>"><i class="bi bi-pencil-square"></i></a></td>
+                                <td><a href="delete.php?maduan=<?php echo $row['maduan']; ?>"><i class="bi bi-trash"></i></a></td>
+                            </tr>
+                <?php
                         }
-                        // Bước 02: Thực hiện truy vấn
-                        $sql = "SELECT nv.maduan, nv.tenduan, nv.namthuchien, nv.linhvuc, nv.nhiemvu, nv.coquanthuchien, FROM bdkh WHERE nv.maduan = dv.maduan";
-                        $result = mysqli_query($conn,$sql);
-                        // Bước 03: Xử lý kết quả truy vấn
-                        if(mysqli_num_rows($result) > 0){
-                            while($row = mysqli_fetch_assoc($result)){
-                    ?>
-                                <tr>
-                                    <th scope="row"><?php echo $row['maduan']; ?></th>
-                                    <td><?php echo $row['tenduan']; ?></td>
-                                    <td><?php echo $row['namthuchien']; ?></td>
-                                    <td><?php echo $row['linhvuc']; ?></td>
-                                    <td><?php echo $row['nhiemvu']; ?></td>
-                                    <td><?php echo $row['coquanthuchien']; ?></td>
-                                    
-                                    <td><a href="update_employee.php?id=<?php echo $row['maduan']; ?>"><i class="bi bi-pencil-square"></i></a></td>
-                                    <td><a href="delete_employee.php?id=<?php echo $row['maduan']; ?>"><i class="bi bi-trash"></i></a></td>
-                                </tr>
-                    <?php
-                            }
-                        }
-                        // Bước 04: Đóng kết nối Database Server
-                        mysqli_close($conn);
-                    ?>
-                    
-                    
-                </tbody>
-                </table>
-        </div>    
+                    }
+                    // Bước 04: Đóng kết nối Database Server
+                    mysqli_close($conn);
+                ?>
+                
+                
+            </tbody>
+            </table>
+    </div>    
     </main>
 
-<?php
-    include("template/footer.php");
-?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
